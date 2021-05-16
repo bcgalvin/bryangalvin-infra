53cb7582400adca7a44774b3253a935051118037
Bryan Galvin <bcgalvin@gmail.com>
2021-05-15 20:25:28 -0700

    fix base tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,13 @@
-import '@aws-cdk/assert/jest';
-import { App } from '@aws-cdk/core';
-import { MyStack } from '../src/main';
+import "@aws-cdk/assert/jest";
+import { App } from "@aws-cdk/core";
+import { AmplifyStack } from "../src/main";
 
-test('Snapshot', () => {
+test("Snapshot", () => {
   const app = new App();
-  const stack = new MyStack(app, 'test');
+  const stack = new AmplifyStack(app, "test");
 
-  expect(stack).not.toHaveResource('AWS::S3::Bucket');
-  expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
+  expect(stack).not.toHaveResource("AWS::S3::Bucket");
+  expect(
+    app.synth().getStackArtifact(stack.artifactId).template
+  ).toMatchSnapshot();
 });
